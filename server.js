2fbdbd950f917448f8f272d240587e48bf723768
akhiln95 <akhilnalliboina@gmail.com>
2020-02-27 01:05:56 +0100

    change somet

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ const db=knex({
   });
 
 
-app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+  app.post('/',(req,res)=>{res.send('its working')})
+  app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
 app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id',(req,res)=>profile.handleProfile(req,res,db))
 app.put('/image',(req,res)=>image.handleImage(req,res,db))
 app.post('/imageurl',(req,res)=>image.handleApiKey(req,res))
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log('app is running on port 3000')
 })
