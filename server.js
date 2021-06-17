16041f1d2d063432145b6ebebc4f8f963e18c378
akhiln95 <akhilnalliboina@gmail.com>
2021-06-17 02:41:59 +0200

    node modules

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.put("/image", (req, res) => image.handleImage(req, res, db));
 app.post("/imageurl", (req, res) => image.handleApiKey(req, res));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("app is running on port ${process.env.PORT}");
+  console.log(`app is running on port ${process.env.PORT}`);
 });
