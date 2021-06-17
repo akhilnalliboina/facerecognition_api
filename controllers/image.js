7471dcb87f2b1dba57b7c220636895257eb0c93f
akhiln95 <akhilnalliboina@gmail.com>
2021-06-17 02:27:15 +0200

    cors update

--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,12 @@
 const Clarifai=require('clarifai')
 
 
-const app = new Clarifai.App({
+const app_clarify = new Clarifai.App({
     apiKey: '4aa73aaeaffa4c0f954ace815c32ca1c'
    });
 
    const handleApiKey=(req,res)=>{
-   app.models
+    app_clarify.models
    .predict(Clarifai.CELEBRITY_MODEL, req.body.input).then(data=>res.json(data).catch(err=>{res.status(400).json('unable to work with API')}))}
 
 const handleImage=(req,res,db)=>{
--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+var bodyParser = require("body-parser");
+const app = express();
 app.use(bodyParser.json());
-var bcrypt = require('bcryptjs');
-const cors = require('cors')
-app.use(cors())
-const knex = require('knex')
-const signin=require('./controllers/signin')
-const register=require('./controllers/register')
-const profile=require('./controllers/profile')
-const image=require('./controllers/image')
+var bcrypt = require("bcryptjs");
+const cors = require("cors");
+app.use(cors());
+const knex = require("knex");
+const signin = require("./controllers/signin");
+const register = require("./controllers/register");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
-const db=knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl:true
-    }
-  });
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+});
 
+app.get("/", (req, res) => {
+  res.send("its working");
+});
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+app.put("/image", (req, res) => image.handleImage(req, res, db));
+app.post("/imageurl", (req, res) => image.handleApiKey(req, res));
 
-app.get('/',(req,res)=>{res.send('its working')})
-app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
-app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
-app.get('/profile/:id',(req,res)=>profile.handleProfile(req,res,db))
-app.put('/image',(req,res)=>image.handleImage(req,res,db))
-app.post('/imageurl',(req,res)=>image.handleApiKey(req,res))
-
-app.listen(process.env.PORT || 3000,()=>{
-    console.log('app is running on port ${process.env.PORT}')
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is running on port ${process.env.PORT}");
+});
